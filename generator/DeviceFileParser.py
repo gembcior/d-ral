67a0bca48196ab74947c9bb31df7e361b8f21446
Gembcior <gembcior@gmail.com>
2021-01-21 23:34:22 +0100

    Work in progress on generator.

--- generator/DeviceFileParser.py
+++ generator/DeviceFileParser.py
@@ -6,23 +6,30 @@ import os
 import re
 
 
-class RegisterFileParser:
+class DeviceFileParser:
     def __init__(self):
         pass
 
-    def _get_peripherals(self, reg_file, peripherals):
-        with open(reg_file, "r") as yaml_file:
-            peripherals.update(yaml.load(yaml_file, Loader=yaml.FullLoader)["peripherals"])
+    def _get_device_data(self, device_file, device_data):
+        with open(device_file, "r") as yaml_file:
+            yaml_data = yaml.load(yaml_file, Loader=yaml.FullLoader)
+            device_data["peripherals"].update(yaml_data["peripherals"])
+            if "brand" in yaml_data:
+                device_data["brand"] = yaml_data["brand"]
+            if "family" in yaml_data:
+                device_data["family"] = yaml_data["family"]
+            if "chip" in yaml_data:
+                device_data["chip"] = yaml_data["chip"]
 
         include_file_list = []
-        with open(reg_file, "r") as yaml_file:
+        with open(device_file, "r") as yaml_file:
             include_file_pattern = re.compile('"(.*?)"')
             for line in yaml_file.readlines():
                 if line.startswith("#include"):
-                    include_file_list.append(os.path.join(os.path.dirname(reg_file), re.findall(include_file_pattern, line)[0]))
+                    include_file_list.append(os.path.join(os.path.dirname(device_file), re.findall(include_file_pattern, line)[0]))
 
         for include_file in include_file_list:
-            self._get_peripherals(include_file, peripherals)
+            self._get_device_data(include_file, device_data)
 
     def _parse_peripherals(self, peripherals):
         peripherals_list = []
@@ -67,9 +74,9 @@ class RegisterFileParser:
             fields_list.append(new_field)
         return fields_list
 
-    def parse(self, reg_file):
-        peripherals = {}
-        self._get_peripherals(reg_file, peripherals)
-        parsed_peripherals_list = self._parse_peripherals(peripherals)
-        return parsed_peripherals_list
+    def parse(self, device_file):
+        device_data = {"peripherals": {}, "brand": None, "family": None, "chip": None}
+        self._get_device_data(device_file, device_data)
+        device_data["peripherals"] = self._parse_peripherals(device_data["peripherals"])
+        return device_data
 
--- generator/Generator.py
+++ generator/Generator.py
@@ -1,15 +1,28 @@
-import RegisterFileParser as register_parser
+import DeviceFileParser as device_file_parser
 import argparse
 import os
 
 
 class Generator:
-    def __init__(self, peripherals):
-        self._peripherals = peripherals
+    def __init__(self):
+        pass
 
-    def generate(self):
-        for item in self._peripherals:
-            print(item.info)
+    def _create_directory(self, brand, family, chip):
+        generator_path = os.path.dirname(os.path.realpath(__file__))
+        directory_path = os.path.join(generator_path, "..", "dral_%s_%s_%s" % (brand, family, chip), brand, family, chip)
+        os.makedirs(directory_path, exist_ok=True)
+        return directory_path
+
+    def _generate_peripheral_file(self, directory, peripheral):
+        peripheral_file = os.path.join(directory, "%s.h" % peripheral.name)
+        content = ["Here will be generated content for this peripheral file.\n", "Stay tuned.\n"]
+        with open(peripheral_file, "w") as new_file:
+            new_file.writelines(content)
+
+    def generate(self, device_data):
+        directory = self._create_directory(device_data["brand"], device_data["family"], device_data["chip"])
+        for peripheral in device_data["peripherals"]:
+            self._generate_peripheral_file(directory, peripheral)
 
 
 def main():
@@ -27,9 +40,9 @@ def main():
     args = parser.parse_args()
 
     generator_path = os.path.dirname(os.path.realpath(__file__))
-    reg_file = os.path.join(generator_path, "..", "devices", args.brand, args.family, "%s.yaml" % args.chip)
-    peripherals = register_parser.RegisterFileParser().parse(reg_file)
-    Generator(peripherals).generate()
+    device_file = os.path.join(generator_path, "..", "devices", args.brand, args.family, "%s.yaml" % args.chip)
+    device_data = device_file_parser.DeviceFileParser().parse(device_file)
+    Generator().generate(device_data)
 
 
 if __name__ == "__main__":
