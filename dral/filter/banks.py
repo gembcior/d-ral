025dded3f52dddb480254d1e44bea66d47648e81
Gembcior <gembcior@gmail.com>
2023-11-02 21:22:01 +0100

    First refactor and clean-up banks filter

--- dral/filter/banks.py
+++ dral/filter/banks.py
@@ -5,67 +5,81 @@ from typing import Any, List, Tuple
 
 from rich.console import Console
 
-from ..types import Bank, Device, Register
+from ..types import Bank, Device, Field, Register
 from .base import BaseFilter
 
 
 class BanksFilter(BaseFilter):
-    # TODO refactor register banks support
-    def _find_register_banks(self, registers: List[Register]) -> List[List[Register]]:  # noqa: C901
-        def get_symmetric_difference(str1: str, str2: str) -> List[str]:
-            from difflib import Differ
-
-            differ = Differ()
-            output = []
-            for item in list(differ.compare(str1, str2)):
-                if "+" in item:
-                    output.append(item.replace("+", "").strip())
-                elif "-" in item:
-                    output.append(item.replace("-", "").strip())
-            return output
-
-        def is_list_of_digits(digits: List[Any]) -> bool:
-            for item in digits:
-                if not item.isdigit():
-                    return False
-            return True
-
-        def only_one_pos(s1: str, s2: str) -> bool:
-            ok = False
-            if len(s1) != len(s2):
-                return False
-            for c1, c2 in zip(s1, s2):
-                if c1 != c2:
-                    if ok:
-                        return False
-                    else:
-                        ok = True
-            return ok
-
-        def compare(item1: Register, item2: Register) -> bool:
-            fields1 = item1.fields
-            fields2 = item2.fields
-            diff = [i for i in fields1 + fields2 if i not in fields1 or i not in fields2]
-            if len(diff) == 0:
-                if item1.name != item2.name:
-                    symmetric_diff = get_symmetric_difference(item1.name, item2.name)
-                    if is_list_of_digits(symmetric_diff):
-                        return True
-                    else:
-                        # TODO
-                        if only_one_pos(item1.name, item2.name):
-                            return True
-                        return False
-                else:
-                    return True
+    def _compare_fields(self, field_a: Field, field_b: Field) -> bool:
+        if field_a.name != field_b.name:
             return False
+        if field_a.position != field_b.position:
+            return False
+        if field_a.width != field_b.width:
+            return False
+        return True
 
+    def _is_field_in_fields(self, field: Field, fields: List[Field]) -> bool:
+        for item in fields:
+            if self._compare_fields(field, item):
+                return True
+        return False
+
+    def _has_the_same_fields(self, register_a: Register, register_b: Register) -> bool:
+        diff = []
+        for field in register_a.fields + register_b.fields:
+            if not self._is_field_in_fields(field, register_a.fields) or not self._is_field_in_fields(field, register_b.fields):
+                diff.append(field)
+        return len(diff) == 0
+
+    def _get_symmetric_difference(self, str1: str, str2: str) -> List[str]:
+        from difflib import Differ
+
+        differ = Differ()
+        output = []
+        for item in list(differ.compare(str1, str2)):
+            if "+" in item:
+                output.append(item.replace("+", "").strip())
+            elif "-" in item:
+                output.append(item.replace("-", "").strip())
+        return output
+
+    def is_list_of_digits(self, digits: List[Any]) -> bool:
+        for item in digits:
+            if not item.isdigit():
+                return False
+        return True
+
+    def only_one_pos(self, s1: str, s2: str) -> bool:
+        ok = False
+        if len(s1) != len(s2):
+            return False
+        for c1, c2 in zip(s1, s2):
+            if c1 != c2:
+                if ok:
+                    return False
+                else:
+                    ok = True
+        return ok
+
+    def _has_similar_name(self, register_a: Register, register_b: Register) -> bool:
+        symmetric_diff = self._get_symmetric_difference(register_a.name, register_b.name)
+        if self.is_list_of_digits(symmetric_diff) or self.only_one_pos(register_a.name, register_b.name):
+            return True
+        return False
+
+    def _compare_registers(self, register_a: Register, register_b: Register) -> bool:
+        same_fields = self._has_the_same_fields(register_a, register_b)
+        similar_name = self._has_similar_name(register_a, register_b)
+        return same_fields and similar_name
+
+    def _find_register_banks(self, registers: List[Register]):
         banks = []
         registers_copy = registers.copy()
         for reg in registers:
             same = []
             for item in registers_copy:
-                if compare(reg, item):
+                if self._compare_registers(reg, item):
                     same.append(item)
             if len(same) > 1:
                 banks.append(same)
