ad01ee2a66267a2e22db41f300dba7bf7cbf868c
Gembcior <gembcior@gmail.com>
2025-07-17 23:18:26 +0200

    Improve running formatters base on Copilot suggestions

--- dral/formatter/cpp.py
+++ dral/formatter/cpp.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import subprocess
-import tempfile
 
 from dral.core.generator import DralOutputFile
 from dral.formatter.base import DralFormatter
@@ -12,13 +11,10 @@ class CppFormatter(DralFormatter):
         if isinstance(objects, DralOutputFile):
             objects = [objects]
         for i, item in enumerate(objects):
-            with tempfile.NamedTemporaryFile(mode="w", encoding="utf-8") as temp:
-                temp.write(item.content)
-                temp.flush()
-                result = subprocess.run(
-                    f"cat {temp.name} | clang-format -style=file:{self._get_style_dir() / '.clang-format'}", shell=True, capture_output=True
-                )
+            result = subprocess.run(
+                ["clang-format", f"-style=file:{self._get_style_dir() / '.clang-format'}"], input=item.content, text=True, capture_output=True
+            )
             if result.returncode != 0:
-                raise RuntimeError(f"Formatting failed for {item.name}: {result.stderr.decode('utf-8')}")
-            objects[i] = DralOutputFile(item.name, result.stdout.decode("utf-8"))
+                raise RuntimeError(f"Formatting failed for {item.name}: {result.stderr}")
+            objects[i] = DralOutputFile(item.name, result.stdout)
         return objects
--- dral/formatter/python.py
+++ dral/formatter/python.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import subprocess
-import tempfile
 
 from dral.core.generator import DralOutputFile
 from dral.formatter.base import DralFormatter
@@ -12,11 +11,10 @@ class PythonFormatter(DralFormatter):
         if isinstance(objects, DralOutputFile):
             objects = [objects]
         for i, item in enumerate(objects):
-            with tempfile.NamedTemporaryFile(mode="w+", encoding="utf-8") as temp:
-                temp.write(item.content)
-                temp.flush()
-                result = subprocess.run(f"cat {temp.name} | ruff --config {self._get_style_dir() / 'ruff.toml'} format -", shell=True, capture_output=True)
+            result = subprocess.run(
+                ["ruff", "--config", str(self._get_style_dir() / "ruff.toml"), "format", "-"], input=item.content, text=True, capture_output=True
+            )
             if result.returncode != 0:
-                raise RuntimeError(f"Formatting failed for {item.name}: {result.stderr.decode('utf-8')}")
-            objects[i] = DralOutputFile(item.name, result.stdout.decode("utf-8"))
+                raise RuntimeError(f"Formatting failed for {item.name}: {result.stderr}")
+            objects[i] = DralOutputFile(item.name, result.stdout)
         return objects
